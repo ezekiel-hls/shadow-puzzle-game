cbc864bf8801d7efb7f4935a3035d1b9ae0bb676
Ezekiel <ezekielm906@gmail.com>
2020-05-23 08:36:26 +0800

    initial commit

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  //Import this
+
+public class GameManagerScript : MonoBehaviour
+{
+    GameObject puzzle_object;
+    PuzzleObjectScript puzzle_script;
+    Text victory_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        puzzle_object = GameObject.Find("PuzzleObject");
+        puzzle_script = puzzle_object.GetComponent<PuzzleObjectScript>();
+
+        victory_text = GameObject.Find("Canvas/Text").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _rotation_x = puzzle_script.rotation_x;
+        float _rotation_y = puzzle_script.rotation_y;
+
+        if(CompareAngle(_rotation_x, 0) && CompareAngle(_rotation_y, 0)){
+            //We win!
+            Debug.Log("Victory");
+
+            //Text
+            victory_text.enabled = true;
+        }
+    }
+
+    //Check if the two given angles are almost similar
+    private bool CompareAngle(float x1, float x2){
+        float xn1 = OppositeAngleDir(x1);
+        if(Mathf.Abs(Mathf.DeltaAngle(x1, x2)) < 1 || Mathf.Abs(Mathf.DeltaAngle(xn1, x2)) < 1){
+            return true;
+        }
+        return false;
+    }
+
+    //Get the opposite direction of an angle
+    private float OppositeAngleDir(float x){ 
+        return((x+180)%360);
+    } 
+}
---
+++ Assets/PuzzleObjectScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleObjectScript : MonoBehaviour
+{
+    public float rotation_speed = 0.04f;
+    public float rotation_x = 0;
+    public float rotation_y = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Randomize starting rotation
+        transform.Rotate(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), 0.0f);
+
+        //
+        GetRotationValue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetRotationValue();
+    }
+
+	void OnMouseDrag()
+	{
+        float _rotation_x = Input.GetAxis("Mouse X")*rotation_speed;
+        float _rotation_y = Input.GetAxis("Mouse Y")*rotation_speed;
+
+        transform.RotateAround(Vector3.down, _rotation_x);
+        transform.RotateAround(Vector3.right, _rotation_y);
+	}
+
+    void GetRotationValue()
+    {
+        rotation_x = gameObject.transform.eulerAngles.x;
+        rotation_y = gameObject.transform.eulerAngles.y;
+    }
+}
